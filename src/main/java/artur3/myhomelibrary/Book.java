1411abd2d360d521758a96fc778ffd7c26643f4f
CORPO\AZajaczkowski <artur.zajaczkowski@t-mobile.pl>
2021-01-12 21:34:24 +0100

    Add new Class Book - @Entity - to manage table DB

---
+++ src/main/java/artur3/myhomelibrary/Book.java
@@ -0,0 +1,47 @@
+package artur3.myhomelibrary;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "library")
+public class Book {
+    @Id
+    private Long id;
+    private String author;
+    private String title;
+
+    public Book() {
+    }
+
+    public Book(Long id, String author, String title) {
+        this.id = id;
+        this.author = author;
+        this.title = title;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
